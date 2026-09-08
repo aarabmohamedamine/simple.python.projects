316f5937cf63d1eee1140ed8dc8d877ffa5ace65
Mohamed Amine <aarabmedamine2@gmail.com>
2026-09-08 03:40:10 +0100

    sqlite upadate

--- to_do_list/operation.py
+++ to_do_list/operation.py
@@ -1,41 +1,100 @@
-from storages import save_tasks
+import sqlite3
+from datetime import datetime 
+def create_table():
+    data = sqlite3.connect("to_do_list/tasks.db")
+    cursor = data.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS task_list(
+        id_user INTEGER PRIMARY KEY AUTOINCREMENT,
+        task TEXT,
+        status TEXT,
+        date TEXT     ) """)
+    data.close()
+
+def add_task():
+    create_table()
+    task_name = input("Enter the task name : ")
+    status = 'Pending'
+    date = datetime.now().strftime("%H:%M")
+    data = sqlite3.connect("to_do_list/tasks.db")
+    cursor = data.cursor()
+    cursor.execute("INSERT INTO task_list(task,status,date) VALUES(?,?,?)",(task_name,status,date))
+    data.commit()
+    print("Task added successfully !")
+    data.close()
 
 
-def add_task(tasks):
+
+def view_tasks():
+    create_table()
+    data = sqlite3.connect("to_do_list/tasks.db")
+    cursor = data.cursor()
+    cursor.execute("SELECT * FROM task_list ")
+    db = cursor.fetchall()
+    if not db:
+        print("List is empty !")
+    else:
+        for row in db:
+            print(row)
     
-    task_name = input('Enter the task : ')
-    task_dictionary = {"Title" : task_name, "status" : "Pending" 
+       
 
-    }
-    tasks.append(task_dictionary)
-    save_tasks(tasks)
-    print("Task added successfully")
-     
-def view_tasks(tasks):
-    for index,task in enumerate(tasks,start=1):
-        print(f"{index} . {task['Title']} - [{task['status']}]")
-    return
-def delete_task(tasks):
-    view_tasks(tasks)
-    index = get_index(tasks)
-    del tasks[index]
-    save_tasks(tasks)
-def mark_done(tasks):
-    
-    view_tasks(tasks)
-    index = get_index(tasks)
-    task_dictionary = tasks[index]
-    task_dictionary['status'] = 'Done'
-    save_tasks(tasks)
+def delete_task():
+    create_table()
+    view_tasks()
+    name = input("Enter the name of the task : ")
+    data = sqlite3.connect("to_do_list/task.db")
+    cursor = data.cursor()
+    cursor.execute("SELECT * FROM task_list WHERE LOWER(task) = LOWER(?)",(name,))
+    db = cursor.fetchall()
+    if not db :
+        print("Task does not exist !")
+    else:
+        cursor.execute("DELETE FROM task_list WHERE LOWER(task) = LOWER(?)",(name,))
+        data.commit()
+        print("Task deleted")
+        data.close()
 
-def get_index(tasks):
-    while True :
-        try :            
-            task_number = int(input("enter the task number : "))
-            if task_number > 0 and task_number <= len(tasks):
-                return task_number-1
-                
-            else :  
-                print('number invalid')
-        except ValueError:
-            print("error.")
+
+
+
+def mark_done():
+    create_table()
+    data = sqlite3.connect("to_do_list/tasks.db")
+    cursor = data.cursor()
+    cursor.execute("SELECT * FROM task_list")
+    db = cursor.fetchall()
+    if not db :
+        print("Liste is empty ! .")
+        data.close()
+    else : 
+        view_tasks()
+        name = input("Enter the name of the task : ")
+        data = sqlite3.connect("to_do_list/tasks.db")
+        cursor = data.cursor()
+        cursor.execute("SELECT * FROM task_list WHERE LOWER(task) = LOWER(?)",(name,))
+        db = cursor.fetchall()
+        if not db :
+            print("Task does not exist !")
+        else:
+            cursor.execute("UPDATE task_list SET status = 'done' WHERE LOWER(task) = LOWER(?)",(name,))
+            data.commit()
+            print("Task mark as done!")
+            data.close()
+  
+def clear_tasks():
+        create_table()
+        data = sqlite3.connect("to_do_list/tasks.db")
+        cursor = data.cursor()
+        cursor.execute("DROP TABLE task_list")
+        data.commit()
+        print("All clear !! ")
+        data.close()
+
+
+
+
+
+
+
+
+
